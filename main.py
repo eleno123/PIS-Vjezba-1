d172eb7bf5064c0b4bb1e60110d538338608e909
eleno123 <eordul00@fesb.hr>
2025-03-19 10:12:27 +0100

    feat: dodana nova metoda

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def main():
 # Ovo je moj komentar - VašeIme
 if __name__ == "__main__":
     main()
-    
+
     def nova_funkcija():
         return "Ovo je nova funkcija."
+    def nova_metoda(self):
+    return "Ovo je nova metoda" 
